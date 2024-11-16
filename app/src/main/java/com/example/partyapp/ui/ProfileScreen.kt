5c47c924549207f6bafed095dbfff3738b038395
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2024-11-16 10:19:45 +0100

    Pick image from device

--- app/src/main/java/com/example/partyapp/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/partyapp/ui/ProfileScreen.kt
@@ -1,6 +1,12 @@
 package com.example.partyapp.ui
 
+import android.net.Uri
 import android.util.Log
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,35 +28,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.example.partyapp.R
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.partyapp.data.entity.User
 import com.example.partyapp.ui.theme.Typography
-import com.example.partyapp.viewModel.EventViewModel
-import com.example.partyapp.viewModel.LocationViewModel
 import com.example.partyapp.viewModel.SettingsViewModel
-import com.example.partyapp.viewModel.UserAddEventViewModel
-import com.example.partyapp.viewModel.UserCreateEventViewModel
-import com.example.partyapp.viewModel.UserScansEventViewModel
 import com.example.partyapp.viewModel.UserViewModel
-import kotlinx.coroutines.flow.first
 
 
-var user: User? = null;
+var user: User? = null
 
 @Composable
 fun ProfileScreen(
@@ -60,7 +56,7 @@ fun ProfileScreen(
     settingsViewModel: SettingsViewModel,
     session: String,
 ) {
-    setCurrentUser(userViewModel, session)
+    SetCurrentUser(userViewModel, session)
     /*
     val context = LocalContext.current
     val users by userViewModel.users.collectAsState(initial = listOf())
@@ -86,11 +82,11 @@ fun ProfileScreen(
                     tint = Color.White
                 )
             }
-            userProfilePic()
+            UserProfilePic()
             Text(text = user?.username ?: "Username", style = Typography.labelMedium)
             Text(text = "City", style = Typography.labelSmall)
             Text(text = "Age", style = Typography.labelSmall)
-            xpBar()
+            XpBar()
             Divider(
                 color = Color.White
             )
@@ -99,12 +95,12 @@ fun ProfileScreen(
 }
 
 @Composable
-fun setCurrentUser(userViewModel: UserViewModel, session: String) {
+fun SetCurrentUser(userViewModel: UserViewModel, session: String) {
     val users by userViewModel.users.collectAsState(initial = listOf())
     if (user == null && users.isNotEmpty()) {
-        if (userViewModel.loggedUser != null) user = userViewModel.loggedUser
-        else if (session != "") user = users.find { it.username == session }
-        else user = users.first()
+        user = if (userViewModel.loggedUser != null) userViewModel.loggedUser
+        else if (session != "") users.find { it.username == session }
+        else users.first()
     }
 
     Log.println(Log.WARN, "USERS_DB", users.size.toString())
@@ -113,7 +109,7 @@ fun setCurrentUser(userViewModel: UserViewModel, session: String) {
 }
 
 @Composable
-fun xpBar() {
+fun XpBar() {
     Row(
         modifier = Modifier.padding(0.dp,15.dp,0.dp,30.dp)
     ) {
@@ -127,31 +123,66 @@ fun xpBar() {
 }
 
 @Composable
-fun userProfilePic() {
+fun UserProfilePic() {
+    var photoUri: Uri? by remember { mutableStateOf(null) }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        Log.d("PHOTO_URI", uri.toString())
+        photoUri = uri
+    }
+
     Box(
         modifier = Modifier.size(160.dp,150.dp)
     ) {
-        AsyncImage(                            //profile picture
-            model = "null",
-            contentDescription = "Profile image",
-            modifier = Modifier
-                .size(130.dp)
-                .clip(CircleShape)
-                .background(Color.Black)
-                .align(Alignment.Center)
-        )
-        SmallFloatingActionButton(
-            onClick = { /*TODO*/ },
-            modifier = Modifier.align(Alignment.TopEnd),
-            containerColor = Color.hsl(0f, 0f, 1f, 0.90f),
-            shape = RoundedCornerShape(40)
-        ) {
-            Icon(
-                imageVector = Icons.Filled.AddAPhoto,
-                contentDescription = "Edit profile",
-                tint = Color.Black,
-                modifier = Modifier.size(20.dp)
+        if (photoUri != null) {
+            //Use Coil to display the selected image
+            val painter = rememberAsyncImagePainter(
+                ImageRequest
+                    .Builder(LocalContext.current)
+                    .data(data = photoUri)
+                    .build()
+            )
+
+            Image(
+                painter = painter,
+                contentDescription = "Profile image",
+                modifier = Modifier
+                    .size(130.dp)
+                    .clip(CircleShape)
+                    .background(Color.Black)
+                    .align(Alignment.Center)
+            )
+        } else {
+            AsyncImage(                            //profile picture
+                model = "null",
+                contentDescription = "Profile image",
+                modifier = Modifier
+                    .size(130.dp)
+                    .clip(CircleShape)
+                    .background(Color.Black)
+                    .align(Alignment.Center)
             )
         }
+        AddImageBtn(launcher = launcher, modifier = Modifier.align(Alignment.TopEnd))
+    }
+}
+
+@Composable
+fun AddImageBtn(launcher: ManagedActivityResultLauncher<PickVisualMediaRequest, Uri?>, modifier: Modifier) {
+    SmallFloatingActionButton(
+        onClick = {
+            launcher.launch(PickVisualMediaRequest(
+                mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+            ))
+        },
+        modifier = modifier,
+        containerColor = Color.hsl(0f, 0f, 1f, 0.90f),
+        shape = RoundedCornerShape(40)
+    ) {
+        Icon(
+            imageVector = Icons.Filled.AddAPhoto,
+            contentDescription = "Edit profile",
+            tint = Color.Black,
+            modifier = Modifier.size(20.dp)
+        )
     }
 }
