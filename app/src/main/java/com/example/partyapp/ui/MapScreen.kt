05bed9294015d12f6028a954d8e8301eccfdb69f
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2024-11-24 13:41:13 +0100

    Change map style

--- app/src/main/java/com/example/partyapp/ui/MapScreen.kt
+++ app/src/main/java/com/example/partyapp/ui/MapScreen.kt
@@ -1,23 +1,25 @@
 package com.example.partyapp.ui
 
 import LocationHelper
-import android.Manifest
+import android.content.Context
 import android.location.Location
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import com.example.partyapp.R
 import com.example.partyapp.services.PermissionsHelper
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapEffect
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
@@ -44,6 +46,7 @@ fun MapScreen() {
 
 @Composable
 fun ShowMapCenteredOn(location: LatLng) {
+    val context = LocalContext.current
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(location, 10f)
     }
@@ -51,12 +54,29 @@ fun ShowMapCenteredOn(location: LatLng) {
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState,
-        uiSettings = MapUiSettings(zoomControlsEnabled = true)
+        uiSettings = MapUiSettings(zoomControlsEnabled = true),
     ) {
+        MapEffect { googleMap ->
+            applyMapStyle(googleMap, context)
+        }
         Marker(
             state = MarkerState(position = location),
             title = "You",
             snippet = "You are here."
         )
+    }
+}
+
+fun applyMapStyle(googleMap: GoogleMap, context: Context) {
+    try {
+        val success = googleMap.setMapStyle(
+            //Created with google maps styling wizard online
+            MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style)
+        )
+        if (!success) {
+            Log.e("MapStyle", "Style parsing failed.")
+        }
+    } catch (e: Exception) {
+        Log.e("MapStyle", "Error loading map style: ${e.localizedMessage}")
     }
 }
