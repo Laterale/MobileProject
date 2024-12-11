9886a905727992f128cbcf87dc94cbb20b99a371
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2024-12-11 18:27:03 +0100

    Show user's events in your event section

--- app/src/main/java/com/example/partyapp/ui/components/MyEventsComponent.kt
+++ app/src/main/java/com/example/partyapp/ui/components/MyEventsComponent.kt
@@ -1,10 +1,13 @@
 package com.example.partyapp.ui.components
 
+import android.util.Log
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,12 +21,16 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.partyapp.data.entity.User
 import com.example.partyapp.services.EventFactory
+import com.example.partyapp.ui.EventCard
 import com.example.partyapp.ui.theme.Typography
 import com.example.partyapp.viewModel.EventViewModel
 
@@ -56,7 +63,11 @@ fun MyEvents(
                 eventViewModel.selectEvent(newEvent)
                 onEventClicked.invoke()
             })
-            ShowTemplateThumbnails(onEventClicked = onEventClicked)
+            ShowTemplateThumbnails(
+                currentUser = currentUser,
+                eventViewModel = eventViewModel,
+                onEventClicked = onEventClicked
+            )
         }
     }
 }
@@ -85,20 +96,30 @@ fun AddEvent(
 
 @Composable
 fun ShowTemplateThumbnails(
+    currentUser: User,
+    eventViewModel: EventViewModel,
     onEventClicked: () -> Unit
 ) {
-    var i = 0
-    while (i != 10) {
+    val events = eventViewModel.events.collectAsState(initial = listOf()).value
+        .filter { it.creator.id == currentUser.id }
+
+    for(event in events) {
         IconButton(
-            onClick = { /*TODO*/ },
+            onClick = onEventClicked,
             modifier = Modifier
                 .padding(5.dp, 0.dp)
                 .width(80.dp)
                 .fillMaxHeight()
                 .clip(RoundedCornerShape(15.dp)),
             colors = IconButtonDefaults.iconButtonColors(Color.hsl(0f, 0f, 1f, 0.10f))
-
-        ) {}
-        i += 1
+        ) {
+            AsyncImage(
+                model = event.image,
+                contentDescription = "event image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+                    .background(Color.Black),
+            )
+        }
     }
 }
