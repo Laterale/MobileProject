4f39d9dc6dfa8c93719b19e2276d8736dd71ab10
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2024-12-12 16:23:53 +0100

    Add theme setting ui

--- app/src/main/java/com/example/partyapp/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/partyapp/ui/SettingsScreen.kt
@@ -2,15 +2,23 @@ package com.example.partyapp.ui
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,7 +26,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.partyapp.ui.theme.Glass10
 import com.example.partyapp.ui.theme.Glass20
+import com.example.partyapp.ui.theme.Indigo
+import com.example.partyapp.ui.theme.Salmon
 import com.example.partyapp.ui.theme.Typography
+import com.example.partyapp.ui.theme.Violet
 import com.example.partyapp.viewModel.UserViewModel
 
 @Composable
@@ -31,8 +42,10 @@ fun SettingsScreen(
             .fillMaxSize()
             .background(Color.Transparent)
             .padding(30.dp, 20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(15.dp)
     ) {
+        ThemeSetting()
         LogoutButton(navigateToLogin, userViewModel)
     }
 }
@@ -57,5 +70,27 @@ fun LogoutButton(
         modifier = Modifier.fillMaxWidth()
     ) {
         Text(text = "Logout", style = Typography.bodyMedium)
+    }
+}
+
+@Preview
+@Composable
+fun ThemeSetting() {
+    var checked by remember { mutableStateOf(false) }
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(text = "Dark mode", color = Color.White)
+        Switch(
+            checked = checked, onCheckedChange = {checked = it},
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = Color.White, checkedTrackColor = Violet,
+                uncheckedThumbColor = Color.White, uncheckedTrackColor = Indigo
+            ),
+            modifier = Modifier
+
+        )
     }
 }
