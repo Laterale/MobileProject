fc6564a2d709f91750d625abf84449058324b4af
Riccardo Tassinari <riccardo.tassinari9@studio.unibo.it>
2025-01-06 18:02:08 +0100

    Added two filters options, one for the date and one for the distance from the user.

--- app/src/main/java/com/example/partyapp/ui/ExploreScreen.kt
+++ app/src/main/java/com/example/partyapp/ui/ExploreScreen.kt
@@ -1,6 +1,8 @@
 package com.example.partyapp.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,16 +10,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.SliderDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Adjust
-import androidx.compose.material.icons.filled.Apartment
 import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderColors
+import androidx.compose.material3.SliderState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -26,12 +34,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.partyapp.ui.components.EventCard
+import com.example.partyapp.ui.components.IconDatePickerComponent
 import com.example.partyapp.viewModel.EventViewModel
 import com.example.partyapp.viewModel.LocationViewModel
 import com.example.partyapp.viewModel.UserViewModel
 import com.example.partyapp.ui.components.PartyDatePickerComponent
+import com.example.partyapp.ui.theme.Glass10
+import com.example.partyapp.ui.theme.Glass20
+import com.example.partyapp.ui.theme.Salmon
 import java.time.ZoneId
 import java.util.Calendar
+import kotlin.math.roundToInt
 
 
 @Composable
@@ -77,51 +90,42 @@ private fun dateToStr(date: Calendar): String {
 
 @Composable
 fun FiltersBar(){
+    val cal = Calendar.getInstance()
     var date: String by remember {
-        mutableStateOf(dateToStr(Calendar.getInstance()))
+        mutableStateOf(dateToStr(cal))
     }
+    var sliderPosition by remember { mutableFloatStateOf(0f) }
     Row(
         modifier = Modifier
             .padding(0.dp, 0.dp, 0.dp, 10.dp)
             .height(25.dp)
     ) {
-        IconButton(
-            onClick = { /*TODO*/ }
-        ) {
-            Icon(
-                imageVector = Icons.Filled.CalendarMonth,
-                contentDescription = "Choose date",
-                tint = Color.White,
-                modifier = Modifier.fillMaxSize()
+        Row(modifier = Modifier.weight(0.4f)) {
+            IconDatePickerComponent(
+                date,
+                onDatePicked = { year, month, day ->
+                    cal.apply { set(year, month, day) }
+                    date = dateToStr(cal)
+                }
             )
         }
-        val cal = Calendar.getInstance()
-        PartyDatePickerComponent(
-            text = date,
-            onDatePicked = { year, month, day ->
-                cal.apply { set(year, month, day) }
-                date = dateToStr(cal)
-            }
-        )
-        IconButton(
-            onClick = { /*TODO*/ }
-        ) {
-            Icon(
-                imageVector = Icons.Filled.Apartment,
-                contentDescription = "Choose place",
-                tint = Color.White,
-                modifier = Modifier.fillMaxSize()
+        Row(modifier = Modifier.weight(0.45f)) {
+            androidx.compose.material.Slider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it.roundToInt().toFloat() },
+                valueRange = 0f..50f,
+                colors = SliderDefaults.colors(
+                    thumbColor = Color.White,
+                    activeTrackColor = Color.White,
+                    inactiveTrackColor = Glass20,
+                )
             )
         }
-        IconButton(
-            onClick = { /*TODO*/ }
+        Row(
+            modifier = Modifier.weight(0.15f),
+            horizontalArrangement = Arrangement.Center
         ) {
-            Icon(
-                imageVector = Icons.Filled.Adjust,
-                contentDescription = "Choose area range",
-                tint = Color.White,
-                modifier = Modifier.fillMaxSize()
-            )
+            Text(text = sliderPosition.roundToInt().toString() + "km")
         }
     }
 }
--- app/src/main/java/com/example/partyapp/ui/components/DatePickerComponent.kt
+++ app/src/main/java/com/example/partyapp/ui/components/DatePickerComponent.kt
@@ -2,19 +2,23 @@ package com.example.partyapp.ui.components
 
 import android.app.DatePickerDialog
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.partyapp.ui.theme.Glass10
-import java.time.format.TextStyle
 import java.util.Calendar
 
 @Composable
@@ -41,5 +45,33 @@ fun PartyDatePickerComponent(
 //            )
     ) {
         Text(text = text, color = Color.White, fontSize = 11.sp)
+    }
+}
+
+@Composable
+fun IconDatePickerComponent(
+    text: String = "",
+    onDatePicked: (Int, Int, Int) -> Unit = {y, m, d -> }
+){
+    val context = LocalContext.current
+    val tp = DatePickerDialog(
+        context,
+        { _, y, m, d -> onDatePicked(y, m, d) },
+        Calendar.getInstance().get(Calendar.YEAR),
+        Calendar.getInstance().get(Calendar.MONTH),
+        Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+    )
+    Row {
+        IconButton(
+            onClick = { tp.show() }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.CalendarMonth,
+                contentDescription = "Choose date",
+                tint = Color.White,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        Text(modifier = Modifier.align(Alignment.Bottom),text = text, color = Color.White,  fontSize = 13.sp)
     }
 }
